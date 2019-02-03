0bd76646f6d4adeb20610e1d4255f5a091af7d3f
xingmimfl <506576511@qq.com>
2019-02-03 15:39:34 +0800

    add files

---
+++ dataset.py
@@ -0,0 +1,128 @@
+# -*- coding: UTF-8 -*-
+import cv2
+import os
+import shutil
+import torch
+import random
+import numpy as np
+from torch.utils.data import Dataset
+from augmentations import *
+import glob
+from config import *
+
+#---a better augment is https://github.com/amdegroot/ssd.pytorch/blob/master/utils/augmentations.py
+
+class ImageSets(Dataset):
+    def __init__(self, isTrain=True, data_path=DATA_PATH, imageSize=12, 
+            files_vec = None, images_vec = None):
+        """
+        files_vec = [pos_12.txt, neg_12.txt, part_12.txt]
+        images_vec = [positive, negative, part] 
+        """
+        self.imageSize = imageSize
+        self.isTrain = isTrain
+
+        images_path_vec = []
+        cls_labels_vec = []
+        angle_labels_vec = []
+        bbox_vec = []
+        for a_file in files_vec:
+            a_file_path = os.path.join(data_path, a_file)
+            for a_line in open(a_file_path):
+                a_line = a_line.strip()
+                array = a_line.split()
+                a_image_path = array[0]
+                a_label = [int(float(array[1]))]
+                a_angle_label = [int(float(array[2]))]
+                a_box = [float(x) for x in array[3:]]
+
+                images_path_vec.append(a_image_path)
+                cls_labels_vec.append(a_label)
+                angle_labels_vec.append(a_angle_label)
+                bbox_vec.append(a_box)
+
+        self.indexes = [x for x in zip(images_path_vec, cls_labels_vec, angle_labels_vec, bbox_vec)] 
+        random.shuffle(self.indexes)  
+        #self.pts_indexes = pts_indexes #---vector of image name
+        #self.images_indexes = images_indexes #---vector of image name
+
+        self.augment = Compose([
+                ConvertFromInts(),
+                PhotometricDistort(),
+                #Resize(imageSize),
+                ValueTrans()
+            ])
+
+    def __len__(self):
+        return len(self.indexes) 
+
+
+    def __getitem__(self, index):
+        a_image_path, a_label, a_angle_label, a_bbox = self.indexes[index]
+        image = cv2.imread(a_image_path) #----be caution, we just consider jpg here
+        a_bbox = np.asarray(a_bbox) #---turn bbox into numpy array
+        a_label = np.asarray(a_label)  #----turn label into numpy array
+        a_angle_label = np.asarray(a_angle_label) #---turn angle label into numpy array
+
+        if self.isTrain:
+            image, a_bbox, a_label = self.augment(image, a_bbox, a_label)   
+
+        image = image.transpose((2, 0, 1)) #---[H, W, C] ----> [C, H, W]
+
+        image = torch.from_numpy(image).float()
+        a_bbox = torch.from_numpy(a_bbox).float() #---turn into tensor
+        a_label = torch.from_numpy(a_label).int()
+        a_angle_label = torch.from_numpy(a_angle_label).int()
+        return image, a_bbox, a_label, a_angle_label, a_image_path
+
+def detection_collate(batch):
+    imgs = []
+    targets = []
+    labels = []
+    angle_labels = []
+    image_paths = []
+    for sample in batch:
+        imgs.append(sample[0])
+        targets.append(sample[1])
+        labels.append(sample[2]) 
+        angle_labels.append(sample[3])
+        image_paths.append(sample[4])
+    return torch.stack(imgs, 0), torch.stack(targets, 0), torch.stack(labels, 0), \
+        torch.stack(angle_labels, 0), image_paths
+
+
+if __name__=="__main__":
+    BATCH_SIZE = 2
+    NUM_WORKERS = 1
+    trainset = ImageSets(isTrain=True, data_path='.', imageSize=256, files_vec=['celeba_label.txt'], images_vec=None) 
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, collate_fn=detection_collate, \
+                    num_workers=NUM_WORKERS, pin_memory=True, drop_last=True)
+
+    target_dir = "tmp_dataset_images"
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    for i_batch, sample_batched in enumerate(train_loader):
+        images = sample_batched[0].numpy()
+        bboxes = sample_batched[1].numpy()
+        labels = sample_batched[2].numpy()
+        angle_labels = sample_batched[3].numpy()
+        image_paths = sample_batched[4]
+        
+        for i in range(BATCH_SIZE):
+            a_image = images[i].transpose((1, 2, 0)) * 255
+            a_image = a_image.astype(np.uint8).copy()
+            height, width, channel = a_image.shape
+            bbox = bboxes[i]
+            bbox[0] = width * bbox[0];
+            bbox[1] = height * bbox[1]
+            bbox[2] = width * bbox[2]
+            bbox[3] = height * bbox[3]             
+            a_image_path = image_paths[i]
+            a_image_name = os.path.basename(a_image_path)
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1); y1 = int(y1)
+            x2 = int(x2); y2 = int(y2)
+            cv2.rectangle(a_image, (x1, y1), (x2, y2), (0,255,0),3)             
+
+            a_image_target_path = os.path.join(target_dir, a_image_name)
+            cv2.imwrite(a_image_target_path, a_image)
---
+++ train_pnet.py
@@ -0,0 +1,139 @@
+import os
+import sys
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.autograd import Variable
+sys.path.append("..")
+from config import *
+import dataset
+import mtcnn
+
+def main():
+    with torch.cuda.device(DEVICE_IDS[0]):
+        p_model = _model_init()
+        #----data loader----
+        train_loader = get_dataset(files_vec=['pos_12.txt', 'neg_12.txt', 'part_12.txt', 'angle_12_aug.txt'])
+        train_iter = iter(train_loader)
+        #----data iter-----
+        check_dir(SNAPSHOT_PATH)
+
+        #----get parameters that need back-propagation
+        params = []
+        for p in list(p_model.parameters()):
+            if p.requires_grad == False: continue
+            params.append(p)
+
+        #----optimizer
+        optimizer = optim.SGD(params, lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
+        scheduler = StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
+        #----training
+        loss_avg = AverageMeter()
+        loss_cls_avg = AverageMeter()
+        loss_bbox_avg = AverageMeter()
+        loss_angle_avg = AverageMeter()
+        acc1 = AverageMeter()
+
+        for i in range(MAX_ITERS):
+            scheduler.step() 
+            #---load data----
+            try:
+                _images, _bbox, _labels, _angle_labels, _image_paths = train_iter.next()
+            except StopIteration:
+                train_iter = iter(train_loader)
+                _images, _bbox, _labels, _angle_labels, _image_paths = train_iter.next() 
+ 
+            #-----training, get loss, and back-propagation
+            _images = Variable(_images.cuda(DEVICE_IDS[0]))
+            _bbox = Variable(_bbox.cuda(DEVICE_IDS[0]))
+
+            _labels = _labels.cuda(DEVICE_IDS[0])
+            _labels_var = Variable(_labels)
+
+            _angle_labels = _angle_labes.cuda(DEVICE_IDS[0])
+            _angle_labels_var = Variable(_angle_labels)
+
+            outputs = p_model(_images) #----model forward 
+            loss_cls, loss_bbox, loss_angle = p_model.get_loss(outputs, _bbox, _labels_var)
+
+            loss = 0
+            if loss_cls is not None:
+                loss += loss_cls
+                loss_cls_avg.update(loss_cls.data[0], BATCH_SIZE)
+                
+            if loss_bbox is not None:
+                loss += loss_cls
+                loss_bbox_avg.update(loss_bbox.data[0], BATCH_SIZE)
+
+            if loss_angle is not None:
+                loss += loss_angle
+                loss_angle_avg.update(loss_angle.data[0], BATCH_SIZE)
+
+            prec1 =  accuracy(outputs[0].data, _labels) 
+            acc1.update(prec1)
+
+            loss_avg.update(loss.data[0], BATCH_SIZE)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i >=TRAIN_OUT_ITER)  and (i % TRAIN_OUT_ITER == 0):  # print every 2000 mini-batches
+                print("iter:%5d " % i, " loss:%.4e" % loss_avg.avg, " loss_cls:%.4e" % loss_cls_avg.avg, 
+                        " loss_bbox:%.4e" % loss_bbox_avg.avg, " loss_angle:%.4e" % loss_angle_avg.avg, " accuracy:%.4e" % acc1.avg)
+
+                save_name = '_'.join([SUFFIX, "iter", str(i), '.model'])                
+                torch.save(p_model, os.path.join(SNAPSHOT_PATH, save_name))
+
+                loss_avg = AverageMeter()
+                loss_cls_avg = AverageMeter()
+                loss_bbox_avg = AverageMeter()
+                loss_angle_avg = AverageMeter()
+                acc1 = AverageMeter()
+                 
+def get_dataset(files_vec=None, images_vec=None):
+    trainset = dataset.ImageSets(isTrain=True, imageSize=12, files_vec=files_vec, images_vec=images_vec)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, collate_fn=dataset.detection_collate, 
+                                                num_workers=NUM_WORKERS, pin_memory=True, drop_last=True)
+    print("length of trainset:\t", len(trainset))
+    return train_loader
+
+     
+def _model_init():
+    p_model = mtcnn.Pnet()
+    #p_model.apply(weight_init)
+    p_model.cuda(DEVICE_IDS[0])
+    p_model.train()
+    return p_model
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target):
+    output = output[:,:,0,0] #---[batch_size, 1, 1, 1] ---> [batch_size, 1] 
+    batch_size = output.size(0)
+    target = target.long() 
+    output = (output >= 0.5).type_as(target)
+    correct = output.eq(target)
+    res = correct.sum() * 100.0 / batch_size 
+    return res
+
+if __name__=="__main__":
+    main()
